37457e16507202597e07678cb9ab6ffd1dd29c53
jacko <churchillj54@gmail.com>
2019-09-05 16:27:36 +0100

    swicting over

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -2,18 +2,19 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ReplyTest extends TestCase
 {
+
+    use DatabaseMigrations;
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * @test
      */
-    public function testExample()
+    public function it_has_an_owner()
     {
         $this->assertTrue(true);
     }
